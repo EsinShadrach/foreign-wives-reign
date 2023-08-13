52e879f493cf7174fdf83668994d8cb2f51b6ace
emsun <emsunconcept@gmail.com>
2023-08-13 22:16:54 +0100

    done with the responsiveness of the kenya page

--- components/kenya-wife/KenyaMeet.tsx
+++ components/kenya-wife/KenyaMeet.tsx
@@ -2,22 +2,24 @@ import Image from 'next/image'
 import React from 'react'
 import KenyaButton from './KenyaButton'
 import CustomButton from '../CustomButton'
+import RippleButton from '@/utils/RippleButton'
 
 function KenyaMeet() {
     return (
-        <section className='pt-[5rem] pb-4'>
-            <div className='w-[80%] mx-auto flex flex-col gap-y-28'>
+        <section className='sm:pt-[5rem] pt-[3rem] pb-4'>
+            <div className='sm:w-[80%] w-[100%] mx-auto flex flex-col  px-4'>
 
 
-                <div className='flex justify-between items-center '>
+                <div className='flex justify-between items-center flex-col sm:flex-row'>
                     <Image src={'/fwr-kenya-connect.png'} width={450} height={400} alt='kenya-trip1' />
-                    <div className='w-[50%] space-y-4'>
-                        <h2 className='text-[3rem] font-bold leading-[3rem]'>Meet friends, join groups and connect</h2>
-                        <p className='font-[400] text-[1.4rem] leading-10 tracking-wider w-[70%]'>with our group and suport feature you can meet peple and connect</p>
-                        {/* <KenyaButton /> */}
-                        <CustomButton >
+                   
+                    <div className='sm:w-[50%] w-[100%] space-y-4 mt-2'>
+                        <h2 className='sm:text-[3rem] text-[2rem] font-bold leading-[3rem]'>Meet friends, join groups and connect</h2>
+                        <p className='font-[400] text-[1.4rem] leading-10 tracking-wider sm:w-[70%] w-[100%]'>with our group and suport feature you can meet peple and connect</p>
+                        
+                        <RippleButton className='border-2 border-[#75A843] rounded-full py-1.5 px-3 transition-all duration-300 shadow-md active:scale-90 hover:scale-90 text-[#75A843] '>
                             Learn more
-                        </CustomButton>
+                        </RippleButton>
                     </div>
                 </div>
                 
